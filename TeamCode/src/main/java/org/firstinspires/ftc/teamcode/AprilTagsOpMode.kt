641b44fdf5612386a07428567ab96c92e9c980f6
lonaslee <93890846+lonaslee@users.noreply.github.com>
2022-10-15 22:54:19 -0500

    pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagsOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagsOpMode.kt
@@ -3,47 +3,23 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import org.firstinspires.ftc.teamcode.AprilTagDetectionPipeline.Tag
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive
-import org.openftc.easyopencv.OpenCvCamera
-import kotlin.run
+import org.firstinspires.ftc.teamcode.pipeline.AprilTagPipeline
 
+fun Telemetry.putfs(fmt: String, vararg args: Any) {
+    addLine(String.format(fmt, args))
+    update()
+}
 
 @Autonomous()
 class AprilTagsOpMode : LinearOpMode() {
     // TODO calibrate
-    val fx = 578.272
-    val fy = 578.272
-    val cx = 402.145
-    val cy = 221.506
-
-    val tagsize = 0.166
+    private val fx = 578.272
+    private val fy = 578.272
+    private val cx = 402.145
+    private val cy = 221.506
+    private val tagsize = 0.166
 
     override fun runOpMode() {
-        fun Telemetry.puts(ln: String) {
-            telemetry.addLine(ln)
-            telemetry.update()
-        }
-
-        val pipeline = AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy)
-        pipeline.init_cam(hardwareMap, telemetry)
-
-        while (!isStarted && !isStopRequested)
-            pipeline.init_loop()
-        if (isStopRequested) return
-
-        // TODO Autonomous run yadayadayada
-
-        when (pipeline.detection) {
-            Tag.LEFT -> run {
-
-            }
-            Tag.MIDDLE, null -> run {
-
-            }
-            Tag.RIGHT -> run {
-
-            }
-        }
+        val pipeline = AprilTagPipeline(telemetry)
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipeline/AprilTagPipeline.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipeline/AprilTagPipeline.kt
@@ -1,45 +1,63 @@
 package org.firstinspires.ftc.teamcode.pipeline
 
+import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.opencv.core.Mat
+import org.opencv.core.Scalar
 import org.opencv.imgproc.Imgproc
-import org.openftc.apriltag.AprilTagDetectorJNI
 import org.openftc.easyopencv.OpenCvPipeline
+import org.openftc.apriltag.AprilTagDetectorJNI as TagDetector
 
-class AprilTagDetector : OpenCvPipeline() {
-
+class AprilTagPipeline(val telemetry: Telemetry) : OpenCvPipeline() {
+    companion object {
+        const val tagsize = 0.166
+        const val fx = 578.272
+        const val fy = 578.272
+        const val cx = 402.145
+        const val cy = 221.506
+        val GREEN = Scalar(0.0, 255.0, 0.0, 255.0)
+    }
 
     enum class Tag(val id: Int) {
-        LEFT(809), MIDDLE(930), RIGHT(1436);
+        LEFT(809), MIDDLE(930), RIGHT(1436), UNKNOWN(-1);
 
         val pos: String = name
 
         companion object {
             const val tagFamily = "tagStandard41h12"
-            fun getTag(id: Int): Tag {
-                return when (id) {
-                    809 -> LEFT
-                    930 -> MIDDLE
-                    1436 -> RIGHT
-                    else -> throw IllegalArgumentException("Invalid getTag() ID.")
-                }
+            fun getTag(id: Int) = when (id) {
+                809 -> LEFT
+                930 -> MIDDLE
+                1436 -> RIGHT
+                else -> UNKNOWN
             }
         }
     }
 
-    private lateinit var greyscale: Mat
-    private val a: Long
 
-    override fun init(mat: Mat?) {
-        a = AprilTagDetectorJNI.createApriltagDetector(Tag.tagFamily, )
+    private val tagDetector_ptr = TagDetector.createApriltagDetector(Tag.tagFamily, 3F, 3)
+    private var greyscale = Mat()
 
-        Imgproc.cvtColor(mat, greyscale, Imgproc.COLOR_RGBA2GRAY)
-        super.init(mat)
-    }
+    protected fun finalize() = TagDetector.releaseApriltagDetector(tagDetector_ptr)
 
-    override fun processFrame(input: Mat?): Mat {
+    var verdict = Tag.UNKNOWN
+    override fun processFrame(input: Mat): Mat {
         Imgproc.cvtColor(input, greyscale, Imgproc.COLOR_RGBA2GRAY)
+        val (detection, tag) = TagDetector.runAprilTagDetectorSimple(
+            tagDetector_ptr, greyscale, tagsize, fx, fy, cx, cy
+        ).takeUnless { it.size == 0 }?.let { Pair(it[0], Tag.getTag(it[0].id)) } ?: Pair(null, null)
 
+        if (detection == null || tag == null) {
+            telemetry.addLine("Not detected. F")
+        } else {
+            telemetry.addLine("Detected tag. \n  ID: ${tag.id}\n  Pos: ${tag.pos}")
+            verdict = tag
+
+            val rad = detection.corners[1].x - detection.corners[0].x
+            Imgproc.circle(input, detection.center, rad.toInt(), GREEN, (rad / 8).toInt())
+            Imgproc.circle(input, detection.center, (rad / 20).toInt(), GREEN, (rad/10).toInt())
+        }
+        telemetry.addLine("Cur/Last: $verdict")
+        telemetry.update()
+        return input
     }
-
-
 }
