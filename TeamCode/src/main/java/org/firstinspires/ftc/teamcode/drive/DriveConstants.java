3b3dc3f6fd23359834c9350b038ec5ec2de10f3a
jonas <lonaslee@github.com>
2022-10-18 18:02:33 -0500

    roadrunner tuning finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1.014191350940985; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 13.5; // in
+    public static double TRACK_WIDTH = 13.88; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
