22967f74ecafa9e5701d1200a8448ba6990baad9
lonaslee <93890846+lonaslee@users.noreply.github.com>
2023-01-14 08:18:24 -0600

    left pose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LeftTraj.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LeftTraj.kt
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.autonomous
 
-import com.acmerobotics.dashboard.config.Config
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm
@@ -13,21 +12,24 @@ import org.firstinspires.ftc.teamcode.robot.subsystems.Claw.States.CLOSED
 import org.firstinspires.ftc.teamcode.robot.subsystems.Claw.States.OPENED
 import org.firstinspires.ftc.teamcode.vision.SignalSleevePipeline.Tag
 
-@Config
+@com.acmerobotics.dashboard.config.Config
 class LeftTraj(
     private val drive: SampleMecanumDrive, private val arm: Arm, private val claw: Claw
 ) {
     companion object {
+        @JvmField var o_x = 48.0
+        @JvmField var o_y = -5.0
+        @JvmField var o_h = 55.0
+
         @JvmField var a_x = 49.0
         @JvmField var a_y = -5.0
         @JvmField var a_h = 55.0
 
-        @JvmField var b_x = 49.0
+        @JvmField var b_x = 50.0
         @JvmField var b_y = -5.0
         @JvmField var b_h = 55.0
 
-        @JvmField var d = 11.5
-        @JvmField var s = 10.0
+        @JvmField var d = 11.0
 
         val endPose = Pose2d(55, 10, 90)
 
@@ -35,33 +37,27 @@ class LeftTraj(
 
         val aPose get() = Pose2d(a_x, a_y, Math.toRadians(a_h))
         val bPose get() = Pose2d(b_x, b_y, Math.toRadians(b_h))
+        val oPose get() = Pose2d(o_x, o_y, Math.toRadians(o_h))
 
         fun Pose2d(x: Int, y: Int, heading: Int) = Pose2d(x.toDouble(), y.toDouble(), heading.rad)
     }
 
-    fun byTag(tag: Tag) = when (tag) {
-        Tag.LEFT -> left
-        Tag.RIGHT -> right
-        else -> middle
-    }
-
-
-    private val coneTraj = drive.trajectorySequenceBuilder(Pose2d())
+    fun byTag(tag: Tag) = drive.trajectorySequenceBuilder(Pose2d())
         .addTemporalMarker { arm.state = MID }
-        .splineToLinearHeading(Pose2d(31, -3, -40), 0.rad)
+        .splineToLinearHeading(Pose2d(30, -3, -40), 0.rad)
         .waitSeconds(0.2)
         .addTemporalMarker { claw.state = OPENED }
         .waitSeconds(0.2)
         .UNSTABLE_addTemporalMarkerOffset(0.3) { arm.state = STACK }
-        .strafeLeft(s)
+        .strafeLeft(10.0)
         // to stack
         .splineToLinearHeading(Pose2d(55, 10, 90), 0.rad)
-        .forward(10.5)
+        .forward(d)
         .waitSeconds(0.1)
         .addTemporalMarker { claw.state = CLOSED }
         .waitSeconds(0.2)
         .addTemporalMarker { arm.state = MID }
-        .lineToSplineHeading(aPose)
+        .lineToSplineHeading(oPose)
         .addTemporalMarker { arm.state = BACKMID }
         .waitSeconds(1.5)
         .addTemporalMarker { claw.state = OPENED }
@@ -69,7 +65,7 @@ class LeftTraj(
         .addTemporalMarker { arm.state = STACK }
         // to stack 2
         .splineToLinearHeading(Pose2d(55, 10, 90), 0.rad)
-        .forward(10.5)
+        .forward(d)
         .waitSeconds(0.1)
         .addTemporalMarker { claw.state = CLOSED }
         .waitSeconds(0.2)
@@ -82,7 +78,7 @@ class LeftTraj(
         .addTemporalMarker { arm.state = STACK }
         // to stack 3
         .splineToLinearHeading(Pose2d(55, 10, 90), 0.rad)
-        .forward(10.5)
+        .forward(d)
         .waitSeconds(0.1)
         .addTemporalMarker { claw.state = CLOSED }
         .waitSeconds(0.2)
@@ -92,14 +88,13 @@ class LeftTraj(
         .waitSeconds(1.5)
         .addTemporalMarker { claw.state = OPENED }
         .waitSeconds(0.2)
-
-    val left = coneTraj.lineToLinearHeading(Pose2d(53, 24, 0))
         .addTemporalMarker { arm.state = GROUND }
-        .build()!!
-    val right = coneTraj.lineToLinearHeading(Pose2d(53, -24, 0))
-        .addTemporalMarker { arm.state = GROUND }
-        .build()!!
-    val middle = coneTraj.lineToLinearHeading(Pose2d(53, 0, 0))
-        .addTemporalMarker { arm.state = GROUND }
+        .lineToLinearHeading(
+            when (tag) {
+                Tag.LEFT -> Pose2d(53, 24, 0)
+                Tag.RIGHT -> Pose2d(54, -24, 0)
+                else -> Pose2d(53, 0, 0)
+            }
+        )
         .build()!!
 }
