bcc6b81a7469ace89e8eec446347498ae18f2bf8
lonaslee <93890846+lonaslee@users.noreply.github.com>
2023-01-21 20:50:52 -0600

    install photon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/EventLoop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/EventLoop.kt
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop
 
+import com.outoftheboxrobotics.photoncore.PhotonCore
+import com.qualcomm.hardware.lynx.LynxModule
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import kotlin.reflect.KProperty
 import kotlin.system.measureNanoTime
@@ -8,6 +10,12 @@ typealias Callback = () -> Unit
 typealias Predicate = () -> Boolean
 
 class EventLoop(private val running: Predicate, private val telemetry: Telemetry? = null) {
+    init {
+        PhotonCore.CONTROL_HUB.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL
+        PhotonCore.EXPANSION_HUB.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL
+        PhotonCore.enable()
+    }
+
     /**
      * [Callback]s that need to be executed on every loop.
      */
@@ -59,11 +67,15 @@ class EventLoop(private val running: Predicate, private val telemetry: Telemetry
      * Start the event loop, which will run blocking until the method passed in the constructor returns false.
      */
     fun run() {
+
         var time = 0L
         while (running()) time = measureNanoTime {
-            telemetry?.addData("hz", 1e9 / time)
             conditionals.filter { it.first() }.forEach { it.second() }
             updates.forEach { it() }
+
+            telemetry?.addData("hz", 1e9 / time)
+            PhotonCore.CONTROL_HUB.clearBulkCache()
+            PhotonCore.EXPANSION_HUB.clearBulkCache()
         }
     }
 }
